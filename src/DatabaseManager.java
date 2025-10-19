8ec4c2f679991a5b066fcefedd85aaaa16850ac6
Roshan17431 <roshanmathew414@gmail.com>
2025-10-19 07:51:10 +0530

    update 6

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final String URL = "jdbc:mysql://localhost:3306/student3";
+    private static final String URL = "jdbc:mysql://localhost:3306/student5";
     private static final String USER = "root";
     private static final String PASSWORD = "roshan17";
 
